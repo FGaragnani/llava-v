61c23d3fccc46e471c1d4a296732e1347cdcc7b5
FGaragnani <filippogaragnani1@gmail.com>
2025-11-22 18:50:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
+import traceback
+
 try:
     from .model import LlavaLlamaForCausalLM
 except ImportError as e:
-    print("Failed to import LlavaLlamaForCausalLM:", e)
+    print("Failed to import LlavaLlamaForCausalLM:")
+    traceback.print_exc()
     raise e
     
