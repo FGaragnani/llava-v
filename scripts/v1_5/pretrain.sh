6712277fc349d9acace76547a749193248417f0b
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-11-24 10:06:20 +0100

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -5,8 +5,8 @@
 #SBATCH --open-mode=truncate
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=4
-#SBATCH --mem=240G
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=10G
 #SBATCH --cpus-per-task=32
 #SBATCH --partition=all_usr_prod
 #SBATCH --account=tesi_fgaragnani
