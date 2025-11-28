ab1ef6c43692ae6e1b12de90de6db901eab01145
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-11-28 16:24:46 +0100

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -97,7 +97,7 @@ srun --exclusive -c $SLURM_CPUS_PER_TASK --mem $SLURM_MEM_PER_NODE \
     --warmup_ratio 0.03 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
     --dataloader_num_workers 4 \
