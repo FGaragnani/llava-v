abdf481471779b2556a7a89751f8f03f9c9ccdbe
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-11-28 15:28:52 +0100

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -104,6 +104,6 @@ srun --exclusive -c $SLURM_CPUS_PER_TASK --mem $SLURM_MEM_PER_NODE \
     --lazy_preprocess True \
     --report_to wandb \
     --use_glamm True \
-    --grand_image_dir /leonardo_scratch/large/userexternal/fgaragna/dataset/GLAMM/images/ \
+    --grand_image_dir /leonardo_scratch/large/userexternal/fgaragna/dataset/GLAMM/images/images/ \
     --grand_annotation_dir /leonardo_scratch/large/userexternal/fgaragna/dataset/GLAMM/annotations/simple/ \
     --patch_agg_mode cls
