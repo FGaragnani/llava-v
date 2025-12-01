32b03d851f10980ca76ec8f140cec0ca70e06c4c
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-12-02 00:54:46 +0100

    Update eval_all.sh

--- scripts/v1_5/eval/eval_all.sh
+++ scripts/v1_5/eval/eval_all.sh
@@ -44,7 +44,7 @@ task_list=(gqa scienceqa_img mmmu_val seedbench ai2d textvqa_val pope mme)
 # task_list=(textvqa_val)
 echo ${task_list[$SLURM_ARRAY_TASK_ID]}
 
-checkpoint_path="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava-v_s2"
+checkpoint_path="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava-v_s2/checkpoint-20500/"
 base_model="/leonardo_scratch/large/userexternal/fgaragna/models/lmsys/vicuna-7b-v1.5"
 
 srun -c $SLURM_CPUS_PER_TASK --mem $SLURM_MEM_PER_NODE \
