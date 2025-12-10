87388a259a70025ded6aa69eff94aa5ba8a111bc
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-12-10 23:54:27 +0100

    Update cambrian.sh

--- scripts/v1_5/eval/cambrian.sh
+++ scripts/v1_5/eval/cambrian.sh
@@ -8,11 +8,10 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=48G
 #SBATCH --cpus-per-task=4
-#SBATCH --partition=all_usr_prod
-#SBATCH --account=tesi_fgaragnani
+#SBATCH --partition=boost_usr_prod
+#SBATCH --account=IscrB_MLLM-RAG
 #SBATCH --time=02:30:00
-#SBATCH --constraint="gpu_A40_45G|gpu_L40S_45G|gpu_RTX6000_24G|gpu_RTX_A5000_24G"
-#SBATCH --array=0-22
+#SBATCH --array=0-7
 
 set -e
 
