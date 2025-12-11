41367c17fbcdca4f586a3d6e66cdda1a47aaa5e0
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-12-11 09:17:42 +0100

    is_llava

--- scripts/v1_5/eval/eval/mme/mme_eval.py
+++ scripts/v1_5/eval/eval/mme/mme_eval.py
@@ -20,7 +20,7 @@ elif IS_ROSS:
     from ross.conversation import conv_templates, SeparatorStyle
     from ross.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
     print("Using imports from ROSS")
-elif True:
+elif IS_LLAVA:
     from llava.model.builder import load_pretrained_model
     from llava.conversation import conv_templates, SeparatorStyle
     from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
