1f569aec917e90917e789b9b8d8d1ef0357aba93
FGaragnani <filippogaragnani1@gmail.com>
2025-12-18 13:37:15 +0100

    Update pretrain.sh

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=llava-v_s1--mean
+#SBATCH --job-name=llava-v_s1--last
 #SBATCH --output=/leonardo_scratch/large/userexternal/fgaragna/logs/%x-%j.out
 #SBATCH --error=/leonardo_scratch/large/userexternal/fgaragna/logs/%x-%j.err
 #SBATCH --open-mode=truncate
@@ -108,4 +108,4 @@ srun --exclusive -c $SLURM_CPUS_PER_TASK --mem $SLURM_MEM_PER_NODE \
     --grand_annotation_dir /leonardo_scratch/large/userexternal/fgaragna/dataset/GLAMM/annotations/simple/ \
     --patch_agg_mode cls \
     --grand_alignment_loss_weight 0.5 \
-    --text_token_pool mean
+    --text_token_pool last
