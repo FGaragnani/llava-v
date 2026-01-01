33eabf69e39f2601e725d38b1bff3461e589b53e
FGaragnani <filippogaragnani1@gmail.com>
2026-01-01 14:16:22 +0100

    Create examine_logs.py

---
+++ data/examine_logs.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import plotext
+
+LOGS_DIR = Path("/leonardo_scratch/large/userexternal/fgaragna/logs")
+
+def main():
+    out_files = sorted(LOGS_DIR.glob("*.out"))
+    for out_file in out_files:
+        print(f"<--- {out_file.name} --->")
+        grand_align_data = []
+        ce_loss_data = []
+        with open(out_file, "r", encoding="utf-8") as f:
+            grand_align_debug = []
+            for line in f:
+                if "[GrandAlignDebug]" in line:
+                    grand_align_debug.append(float(line.split("=")[1]))
+                else:
+                    ce_loss_data.append(float(line.split(":")[1].split(",")[0]))
+                    grand_align_data.append(sum(grand_align_debug)/len(grand_align_debug) if grand_align_debug else 0.0)
+                    grand_align_debug = []
+        plotext.clf()
+        plotext.plot(grand_align_data, label="GrandAlignDebug")
+        plotext.plot(ce_loss_data, label="CE Loss")
+        plotext.title(f"Logs from {out_file.name}")
+        plotext.xlabel("Iterations")
+        plotext.ylabel("Value")
+        plotext.legend()
+        plotext.show()
+
+if __name__ == "__main__":
+    main()
