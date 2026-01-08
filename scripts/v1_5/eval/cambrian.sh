67ab30ff43fbe5a71a9cb310d1e4eba58f5a3c2e
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-01-08 19:33:30 +0100

    Update cambrian.sh

--- scripts/v1_5/eval/cambrian.sh
+++ scripts/v1_5/eval/cambrian.sh
@@ -38,10 +38,10 @@ export IS_LLAVA_MORE=1
 PROJECT_ROOT="$HOME"
 export PYTHONPATH="$PROJECT_ROOT/llava:$PROJECT_ROOT:$PYTHONPATH"
 
-llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava--only-GLAMM/" # <--
+llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava--only-GLAMM-align/" # <--
 model_name="${1:-${llava_more}}"
 conv_mode="${2:-vicuna_v1}"
-eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava--only-GLAMM" # <--
+eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava--only-GLAMM-align" # <--
 gpu_devices="${3:-0}"
 safe_model_name=$(tr '/' '_' <<< $model_name)
 
