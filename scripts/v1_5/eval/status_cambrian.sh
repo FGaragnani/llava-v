337a9b508a42d831d5c468c3563b7b73ea35d46d
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-01-09 10:25:13 +0100

    Update status_cambrian.sh

--- scripts/v1_5/eval/status_cambrian.sh
+++ scripts/v1_5/eval/status_cambrian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Configuration - adjust these paths to match your setup
-eval_output_dir="${2:-/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava--only-GLAMM}"
+eval_output_dir="${2:-/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava--only-GLAMM-align}"
 _model_name="LLaVA-Last"
 model_name="${1:-${_model_name}}"
 echo "Model name: ${model_name}"
