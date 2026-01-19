954c89a0adc3da9c5b0f130e529dea54cff65e96
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-01-19 16:46:56 +0100

    Update scp_model.sh

--- scp_model.sh
+++ scp_model.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+file="model_list.txt"
+
 while IFS= read -r model_name; do
     echo "Processing: $model_name"
     
-    mkdir /work/tesi_fgaragnani/checkpoints/llava-v/"$model_name"
-    cd /work/tesi_fgaragnani/checkpoints/llava-v/"$model_name"
+    mkdir -p /work/tesi_fgaragnani/checkpoints/llava-v/"$model_name"
+    cd /work/tesi_fgaragnani/checkpoints/llava-v/"$model_name" || { echo "Failed to cd to /work/tesi_fgaragnani/checkpoints/llava-v/$model_name"; continue; }
     scp fgaragna@login.leonardo.cineca.it:/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/"$model_name"/* .
     echo "Completed copying for: $model_name"
     
-done
+done < "$file"
