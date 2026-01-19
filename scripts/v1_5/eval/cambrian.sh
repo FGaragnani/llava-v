cc20bd8ab898c3de65ee941c15b967de19a37ca3
FGaragnani <filippogaragnani1@gmail.com>
2026-01-19 09:13:01 +0100

    Update cambrian.sh

--- scripts/v1_5/eval/cambrian.sh
+++ scripts/v1_5/eval/cambrian.sh
@@ -44,10 +44,10 @@ export PYTHONPATH="$PROJECT_ROOT/llava:$PROJECT_ROOT:$PYTHONPATH"
 # drwxr-xr-x.  3 fgaragna interactive 4096 Jan 19 00:03 llava-v_s2--mean-1
 # drwxr-xr-x.  3 fgaragna interactive 4096 Jan 19 00:03 llava--mean-midL-full
 
-llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava-v_s2--mean-midL-1/" # <--
+llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava--mean-midL-full/" # <--
 model_name="${1:-${llava_more}}"
 conv_mode="${2:-llava_v1}"
-eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava-v--mean-midL-1" # <--
+eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava-v--mean-midL-full" # <--
 gpu_devices="${3:-0}"
 safe_model_name=$(tr '/' '_' <<< $model_name)
 
