320053e36a790eed0f7d1a604b126712554f9202
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-02-05 20:12:32 +0100

    multicambrian

--- scripts/v1_5/eval/cambrian.sh
+++ scripts/v1_5/eval/cambrian.sh
@@ -38,12 +38,10 @@ export IS_LLAVA_MORE=1
 PROJECT_ROOT="$HOME"
 export PYTHONPATH="$PROJECT_ROOT/llava:$PROJECT_ROOT:$PYTHONPATH"
 
-# drwxr-xr-x.  3 fgaragna interactive 4096 Jan 19 00:04 llava--only-GLAMM--mean-midL-full
-
-llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava--only-GLAMM--mean-full/" # <--
 model_name="${1:-${llava_more}}"
+llava_more="/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/${model_name}" # <--
 conv_mode="${2:-llava_v1}"
-eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-llava--only-GLAMM--mean-full" # <--
+eval_output_dir="/leonardo_scratch/large/userexternal/fgaragna/logs/cambrian-eval-${model_name}" # <--
 gpu_devices="${3:-0}"
 safe_model_name=$(tr '/' '_' <<< $model_name)
 
---
+++ scripts/v1_5/eval/multi-cambrian.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cambrian.sh llava-v_s2--mean-midL
+cambrian.sh llava-v_s2--mean
+cambrian.sh llava-v_s2--last
+cambrian.sh llava-v_s2--last-midL
