5007983d13a3d7680beb9d9553f4bbfe44ac9c53
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-02-05 23:39:06 +0100

    Update multi-cambrian.sh

--- scripts/v1_5/eval/multi-cambrian.sh
+++ scripts/v1_5/eval/multi-cambrian.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean-midL
-# sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean
-# sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--last
-# sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--last-midL
+sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean
+sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--last
+sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--last-midL
