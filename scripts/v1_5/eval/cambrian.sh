4325c3247e214a1dd1631a4b6e689d9067d4dd8f
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-02-15 22:50:03 +0100

    cambrian

--- scripts/v1_5/eval/cambrian.sh
+++ scripts/v1_5/eval/cambrian.sh
@@ -11,7 +11,7 @@
 #SBATCH --partition=boost_usr_prod
 #SBATCH --account=AIFAC_S02_096
 #SBATCH --time=02:30:00
-#SBATCH --array=0-20
+#SBATCH --array=0-0
 
 set -e
 
@@ -60,34 +60,7 @@ export CUDA_VISIBLE_DEVICES="$gpu_devices"
 
 # All Cambrian benchmarks
 benchmarks=(
-    gqa
-    vizwiz
-    scienceqa
-    textvqa
-    pope
-    mme
-    mmbench_en
-    mmbench_cn
     seed
-    # mmvet
-    mmmu
-    mathvista
-    ai2d
-    chartqa
-    # docvqa
-    # infovqa
-    # stvqa
-    ocrbench
-    mmstar
-    realworldqa
-    # qbench
-    blink
-    mmvp
-    vstar
-    ade
-    # omni
-    coco
-    # synthdog
 )
 
 # Select benchmark based on array task ID
