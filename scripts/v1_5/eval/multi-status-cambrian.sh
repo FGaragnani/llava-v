712ccfa3fe839cc96be7f42d461bbdec41cf011a
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-02-15 23:41:30 +0100

    upd

--- scripts/v1_5/eval/multi-status-cambrian.sh
+++ scripts/v1_5/eval/multi-status-cambrian.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-bash scripts/v1_5/eval/status_cambrian.sh llava-v_s2--mean-midL-full-iti
 bash scripts/v1_5/eval/status_cambrian.sh llava--only-GLAMM--mean-midL-full
 bash scripts/v1_5/eval/status_cambrian.sh llava--only-GLAMM--mean-full
