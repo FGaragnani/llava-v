007442a2c631b3d2d91f614511f265448dea3c81
FGaragnani <filippogaragnani1@gmail.com>
2026-03-09 14:29:43 +0100

    Update multi-cambrian.sh

--- scripts/v1_5/eval/multi-cambrian.sh
+++ scripts/v1_5/eval/multi-cambrian.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean-midL_ve-qwen2_5 qwen2_5
-sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean-midL_ve-siglip
+sbatch scripts/v1_5/eval/cambrian.sh llava-v_s2--mean-midL_ve-qwen2_5
