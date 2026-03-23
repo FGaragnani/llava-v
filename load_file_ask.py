a60951471c5591ebf8d5794de599152fccc6a60c
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-03-23 14:55:57 +0100

    Create load_file_ask.py

---
+++ load_file_ask.py
@@ -0,0 +1,24 @@
+# Utility file. Load a model with LLaVAModelForCausalLM and ask a question.
+
+from model.language_model.llava_llama import LLaVAModelForCausalLM
+
+def load_model(model_path):
+    model = LLaVAModelForCausalLM.from_pretrained(model_path)
+    return model
+
+def ask(model, question):
+    inputs = model.tokenizer(question, return_tensors="pt").to(model.device)
+    outputs = model.generate(**inputs, max_length=100)
+    answer = model.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return answer
+
+MODEL_PATH = ""
+
+input_str = ""
+
+while True:
+    input_str = input("Enter your question (Ctrl-D to exit): ")
+    if input_str == u'\u0004':
+        break
+    answer = ask(load_model(MODEL_PATH), input_str)
+    print("Answer:", answer)
