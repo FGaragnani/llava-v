26533ed8744d11cba4dfa34fa9f018dd5415834d
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-03-23 15:10:31 +0100

    Update load_file_ask.py

--- load_file_ask.py
+++ load_file_ask.py
@@ -3,6 +3,9 @@
 import sys
 sys.path.insert(0, '/leonardo/home/userexternal/fgaragna')
 
+import torch
+from transformers import AutoTokenizer
+
 from model.language_model.llava_llama import LlavaLlamaForCausalLM
 from model.language_model.llava_qwen import LlavaQwenForCausalLM
 
@@ -13,15 +16,23 @@ def load_model(model_path):
         model = LlavaLlamaForCausalLM.from_pretrained(model_path)
     return model
 
-def ask(model, question):
-    inputs = model.tokenizer(question, return_tensors="pt").to(model.device)
-    outputs = model.generate(**inputs, max_length=100)
-    answer = model.tokenizer.decode(outputs[0], skip_special_tokens=True)
+def load_tokenizer(model_path):
+    return AutoTokenizer.from_pretrained(model_path, use_fast=False)
+
+def ask(model, tokenizer, question):
+    device = next(model.parameters()).device
+    inputs = tokenizer(question, return_tensors="pt").to(device)
+    with torch.inference_mode():
+        outputs = model.generate(**inputs, max_new_tokens=100)
+    new_tokens = outputs[0][inputs["input_ids"].shape[1]:]
+    answer = tokenizer.decode(new_tokens, skip_special_tokens=True)
     return answer
 
 
 MODEL_PATH = "/leonardo_scratch/large/userexternal/fgaragna/checkpoints/llava-v/llava-v_s2--ve-qwen2_5"
 model = load_model(MODEL_PATH)
+model.eval()
+tokenizer = load_tokenizer(MODEL_PATH)
 
 input_str = ""
 
@@ -29,5 +40,5 @@ while True:
     input_str = input("Enter your question (Ctrl-D to exit): ")
     if input_str == u'\u0004':
         break
-    answer = ask(model, input_str)
+    answer = ask(model, tokenizer, input_str)
     print("Answer:", answer)
