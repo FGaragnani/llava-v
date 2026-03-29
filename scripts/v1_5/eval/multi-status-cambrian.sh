90e2e89bdc6b2ae259c5433846db9eb218dcc8ee
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-03-29 16:23:24 +0200

    Update multi-status-cambrian.sh

--- scripts/v1_5/eval/multi-status-cambrian.sh
+++ scripts/v1_5/eval/multi-status-cambrian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_mean-midL_siglip
-bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_mean-midL_clip
+bash scripts/v1_5/eval/status_cambrian.sh llava-v_s2--mean_24L
+bash scripts/v1_5/eval/status_cambrian.sh llava-v_s2--mean_1L
