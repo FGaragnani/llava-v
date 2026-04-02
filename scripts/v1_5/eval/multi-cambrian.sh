96ad44f84b3109022c685c6dc7cd688f065f0850
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2026-04-02 23:46:03 +0200

    Update multi-cambrian.sh

--- scripts/v1_5/eval/multi-cambrian.sh
+++ scripts/v1_5/eval/multi-cambrian.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-sbatch scripts/v1_5/eval/cambrian.sh stage_three/llava-base_s3--align_mean-midL
-sbatch scripts/v1_5/eval/cambrian.sh stage_three/llava-base_s3--align_mean-midL_clip
-sbatch scripts/v1_5/eval/cambrian.sh stage_three/llava-base_s3--align_mean-midL_siglip
 sbatch scripts/v1_5/eval/cambrian.sh stage_three/llava-base_s3--align_extra-data
