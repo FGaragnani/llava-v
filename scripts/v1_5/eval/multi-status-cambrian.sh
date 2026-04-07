a4f2ef2938f1c1aeeb01ccfd67d013e49d303382
FGaragnani <filippogaragnani1@gmail.com>
2026-04-07 16:01:27 +0200

    Update multi-status-cambrian.sh

--- scripts/v1_5/eval/multi-status-cambrian.sh
+++ scripts/v1_5/eval/multi-status-cambrian.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-bash scripts/v1_5/eval/status_cambrian.sh llava-v_s2--mean_24L
-bash scripts/v1_5/eval/status_cambrian.sh llava-v_s2--mean_1L
+bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_extra-data
+bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_mean-midL
+bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_mean-midL_clip
+bash scripts/v1_5/eval/status_cambrian.sh stage_three/llava-base_s3--align_mean-midL_siglip
